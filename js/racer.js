91982d6bcdb6ac465f93b325fd72f850ad3078e2
howardsmithnz <howardsmithnz@gmail.com>
2016-01-16 23:34:54 +1300

    basic game play going

--- js/racer.js
+++ js/racer.js
@@ -1,3 +1,29 @@
 // racer.js - a drag racing game in JS
 // by Howard Smith 16/01/2016 AD
+// x = 88
+// o = 79
+
+$(document).ready(function() {
+  function doStuff() {
+ // $(".racer_table #player1_strip .active").html("X");
+  $(".racer_table #player1_strip .active").next().addClass("active").html("X");
+  }
+
+  function doOtherStuff() {
+   // $(".racer_table #player2_strip .active").html("O");
+    $(".racer_table #player2_strip .active").next().addClass("active").html("O");
+  }
+
+  //$("button#goButton").click(doStuff);
+  //$("button#otherGoButton").click(doOtherStuff);
+
+  $(document).keyup(function(e){
+    if(event.which == 88) {
+      doStuff();
+    } else
+    if(event.which == 79) {
+      doOtherStuff();
+    }
+  });
+});
 
