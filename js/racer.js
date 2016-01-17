6892223862875e7ac988f1b4e5edcd32fa6c1006
howardsmithnz <howardsmithnz@gmail.com>
2016-01-17 18:50:23 +1300

    add Finish event, Instructions, and Reset button

--- js/racer.js
+++ js/racer.js
@@ -1,20 +1,29 @@
 // racer.js - a drag racing game in JS
 // by Howard Smith 16/01/2016 AD
-// x = 88
-// o = 79
+// char x = 88
+// char o = 79
 
 $(document).ready(function() {
   function doStuff() {
  // $(".racer_table #player1_strip .active").html("X");
-  $(".racer_table #player1_strip .active").next().addClass("active").html("X");
+    $(".racer_table #player1_strip .active").next().addClass("active").html("X");
+    if($("#finish_line_x").hasClass("active") ) {
+      alert("X wins!");
+    }
   }
 
   function doOtherStuff() {
    // $(".racer_table #player2_strip .active").html("O");
     $(".racer_table #player2_strip .active").next().addClass("active").html("O");
+    if($("#finish_line_o").hasClass("active") ) {
+      alert("O wins!");
+    }
   }
 
-  //$("button#goButton").click(doStuff);
+  $("button#resetButton").click(function() {
+      alert("OK, I'm resetting");
+      location.reload();
+    });
   //$("button#otherGoButton").click(doOtherStuff);
 
   $(document).keyup(function(e){
